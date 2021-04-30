c2fcb8b71a25aab156ecccab9547b605cd17dbe4
Juliettejns <56683417+Juliettejns@users.noreply.github.com>
2021-04-30 12:04:29 +0200

    Update build_train_alto.sh

--- build_train_alto.sh
+++ build_train_alto.sh
@@ -5,37 +5,37 @@ rm -r trainingData; mkdir trainingData
 #if [ -d trainingData ]; then rm -Rf trainingData; fi
 mkdir -p trainingData
 #getting images
-cp 1_Data/*/*png trainingData
-cp 1_Data/*/*jpg trainingData
-cp 1_Data/*/*tif trainingData
+cp 1_Data/*/data_segmenteur/images/*png trainingData
+cp 1_Data/*/data_segmenteur/images/*jpg trainingData
+cp 1_Data/*/data_segmenteur/images/*tif trainingData
 #getting the ALTO4 files
-cp 1_Data/*/*xml trainingData
+cp 1_Data/*/data_segmenteur/alto_xml/*xml trainingData
 
 #creating training data using only the annuaires dataset
 #!/bin/bash
 rm -r trainingDataAnnuaire; mkdir trainingDataAnnuaire
 mkdir -p trainingDataAnnuaire
 #getting images
-cp 1_Data/Annuaires/*tif trainingDataAnnuaire
+cp 1_Data/Annuaires/data_segmenteur/images/*tif trainingDataAnnuaire
 #getting the ALTO4 files
-cp 1_Data/Annuaires/*xml trainingDataAnnuaire
+cp 1_Data/Annuaires/data_segmenteur/alto_xml/*xml trainingDataAnnuaire
 
 #creating training data using only the exhibition catalogs dataset
 #!/bin/bash
 rm -r trainingDataExhibition; mkdir trainingDataExhibition
 mkdir -p trainingDataExhibition
 #getting images
-cp 1_Data/Cat_exhibition/*png trainingDataExhibition
-cp 1_Data/Cat_exhibition/*jpg trainingDataExhibition
+cp 1_Data/Cat_exhibition/data_segmenteur/images/*png trainingDataExhibition
+cp 1_Data/Cat_exhibition/data_segmenteur/images/*jpg trainingDataExhibition
 #getting the ALTO4 files
-cp 1_Data/Cat_exhibition/*xml trainingDataExhibition
+cp 1_Data/Cat_exhibition/data_segmenteur/alto_xml/*xml trainingDataExhibition
 
 #creating training data using only the manuscripts catalogs dataset
 #!/bin/bash
 rm -r trainingDataManuscript; mkdir trainingDataManuscript
 mkdir -p trainingDataManuscript
 #getting images
-cp 1_Data/Cat_manuscripts/*png trainingDataManuscript
-cp 1_Data/Cat_manuscripts/*jpg trainingDataManuscript
+cp 1_Data/Cat_manuscripts/data_segmenteur/images/*png trainingDataManuscript
+cp 1_Data/Cat_manuscripts/data_segmenteur/images/*jpg trainingDataManuscript
 #getting the ALTO4 files
-cp 1_Data/Cat_manuscripts/*xml trainingDataManuscript
+cp 1_Data/Cat_manuscripts/data_segmenteur/alto_xml/*xml trainingDataManuscript
