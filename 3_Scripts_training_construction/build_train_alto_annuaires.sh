a2072117d068ca04edfe462d15aa4c2de986d2ec
Juliettejns <56683417+Juliettejns@users.noreply.github.com>
2021-04-30 12:19:24 +0200

    Update build_train_alto_annuaires.sh

--- 3_Scripts_training_construction/build_train_alto_annuaires.sh
+++ 3_Scripts_training_construction/build_train_alto_annuaires.sh
@@ -1,1 +1,10 @@
 
+#creating training data using only the annuaires dataset
+#!/bin/bash
+rm -r trainingDataAnnuaire; mkdir trainingDataAnnuaire
+mkdir -p trainingDataAnnuaire
+#getting images
+cp 1_Data/Annuaires/data_segmenteur/images/*tif trainingDataAnnuaire
+#getting the ALTO4 files
+cp 1_Data/Annuaires/data_segmenteur/alto_xml/*xml trainingDataAnnuaire
+
