d438149596d00ddb6ccf88e797cf31fdf101e6b0
Juliettejns <56683417+Juliettejns@users.noreply.github.com>
2021-04-30 12:19:45 +0200

    Update build_train_alto_exhibition.sh

--- 3_Scripts_training_construction/build_train_alto_exhibition.sh
+++ 3_Scripts_training_construction/build_train_alto_exhibition.sh
@@ -1,1 +1,10 @@
+#creating training data using only the exhibition catalogs dataset
+#!/bin/bash
+rm -r trainingDataExhibition; mkdir trainingDataExhibition
+mkdir -p trainingDataExhibition
+#getting images
+cp 1_Data/Cat_exhibition/data_segmenteur/images/*png trainingDataExhibition
+cp 1_Data/Cat_exhibition/data_segmenteur/images/*jpg trainingDataExhibition
+#getting the ALTO4 files
+cp 1_Data/Cat_exhibition/data_segmenteur/alto_xml/*xml trainingDataExhibition
 
