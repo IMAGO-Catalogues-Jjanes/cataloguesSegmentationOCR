63c6016fd949553a016acaae55ab6d2581a2aa6d
Juliettejns <56683417+Juliettejns@users.noreply.github.com>
2021-04-30 12:20:00 +0200

    Update build_train_alto_manuscripts.sh

--- 3_Scripts_training_construction/build_train_alto_manuscripts.sh
+++ 3_Scripts_training_construction/build_train_alto_manuscripts.sh
@@ -1,1 +1,11 @@
 
+#creating training data using only the manuscripts catalogs dataset
+#!/bin/bash
+rm -r trainingDataManuscript; mkdir trainingDataManuscript
+mkdir -p trainingDataManuscript
+#getting images
+cp 1_Data/Cat_manuscripts/data_segmenteur/images/*png trainingDataManuscript
+cp 1_Data/Cat_manuscripts/data_segmenteur/images/*jpg trainingDataManuscript
+#getting the ALTO4 files
+cp 1_Data/Cat_manuscripts/data_segmenteur/alto_xml/*xml trainingDataManuscript
+
