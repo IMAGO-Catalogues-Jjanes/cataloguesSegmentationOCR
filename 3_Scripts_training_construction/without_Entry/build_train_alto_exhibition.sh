19201b79320541ea257ff67c34e562a3e5686140
Juliettejns <56683417+Juliettejns@users.noreply.github.com>
2021-05-27 16:39:45 +0200

    Update build_train_alto_exhibition.sh

--- 3_Scripts_training_construction/without_Entry/build_train_alto_exhibition.sh
+++ 3_Scripts_training_construction/without_Entry/build_train_alto_exhibition.sh
@@ -3,8 +3,8 @@
 rm -r trainingDataExhibition; mkdir trainingDataExhibition
 mkdir -p trainingDataExhibition
 #getting images
-cp ../1_Data/Cat_exhibition/images/*png trainingDataExhibition
-cp ../1_Data/Cat_exhibition/images/*jpg trainingDataExhibition
+cp .../1_Data/Cat_exhibition/images/*png trainingDataExhibition
+cp ..//1_Data/Cat_exhibition/images/*jpg trainingDataExhibition
 #getting the ALTO4 files
-cp ../1_Data/Cat_exhibition/alto_eScriptorium/*xml trainingDataExhibition
+cp .../1_Data/Cat_exhibition/alto_eScriptorium_Simple/*xml trainingDataExhibition
 
