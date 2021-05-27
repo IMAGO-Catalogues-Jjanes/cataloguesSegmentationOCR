1e88cce5ed0486767cb3735e24d0c7840ed3d3cf
Juliettejns <56683417+Juliettejns@users.noreply.github.com>
2021-05-27 16:42:05 +0200

    Update build_train_alto.sh

--- 3_Scripts_training_construction/with_Entry/build_train_alto.sh
+++ 3_Scripts_training_construction/with_Entry/build_train_alto.sh
@@ -1,12 +1,12 @@
 #creating training data using all the dataset
 #!/bin/bash
 #Delete dir if it exists
-rm -r trainingData; mkdir trainingData
+rm -r trainingDataEntry; mkdir trainingDataEntry
 #if [ -d trainingData ]; then rm -Rf trainingData; fi
-mkdir -p trainingData
+mkdir -p trainingDataEntry
 #getting images
-cp .../1_Data/*/images/*png trainingData
-cp .../1_Data/*/images/*jpg trainingData
-cp .../1_Data/*/images/*tif trainingData
+cp .../1_Data/*/images/*png trainingDataEntry
+cp .../1_Data/*/images/*jpg trainingDataEntry
+cp .../1_Data/*/images/*tif trainingDataEntry
 #getting the ALTO4 files
-cp .../1_Data/*/alto_eScriptorium_Simple/*xml trainingData
+cp .../1_Data/*/alto_eScriptorium_Entry/*xml trainingDataEntry
