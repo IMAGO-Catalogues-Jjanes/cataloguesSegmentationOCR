f0662494210683f60e6e4a7477a95845f1f01e30
Juliettejns <56683417+Juliettejns@users.noreply.github.com>
2021-06-09 17:12:52 +0200

    Update random_data.py

--- 3_Scripts_training_construction/random_data.py
+++ 3_Scripts_training_construction/random_data.py
@@ -4,18 +4,15 @@ import random
 if __name__ == '__main__':
 
     train = open("train.txt", "w")
-    test = open("dev.txt", "w")
-    val = open("val.txt", "w")
+    test = open("test.txt", "w")
     random.seed('1214')
 
     for i in sys.argv[1:]:
         monRand = random.random()
         if monRand <= 0.1:
-            dev.write(i+"\n")
-        elif 0.1<monRand<0.2:
-            dev.write(i+"\n")
+            test.write(i+"\n")
         else:
             train.write(i+"\n")
 
     train.close()
-    dev.close()
+    test.close()
