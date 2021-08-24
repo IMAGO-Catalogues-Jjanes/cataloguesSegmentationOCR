9509c2c262c58384e3f15a03684969727f555804
Juliettejns <56683417+Juliettejns@users.noreply.github.com>
2021-08-24 11:53:45 +0000

    correction random_data

---
+++ 3_Scripts_training_construction/random_data.py
@@ -0,0 +1,28 @@
+import sys
+import random
+import os
+
+if __name__ == '__main__':
+    test = input("""Choose the test you want for your dataset: 
+                    test_15.txt for a test dataset of 15 pages (5 of each documents types)
+                    test_30.txt for a test dataset of 30 pages (10 of each documents types)
+                    test_20_CatExpo.txt for a test dataset of 20 exhibition catalogs' pages
+                    """)
+    train = open("train.txt", "w")
+    val = open("val.txt", "w")
+    random.seed('1214')
+
+    for i in os.listdir("./trainingData/"):
+        with open(test) as f:
+            if i in f.read():
+                pass
+            else:
+                monRand = random.random()
+                if monRand <= 0.1:
+                    val.write("./trainingData/"+i+"\n")
+                else:
+                    train.write("./trainingData/"+i+"\n")
+
+    train.close()
+    val.close()
+    print("Dataset created")
